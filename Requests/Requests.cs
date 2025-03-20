3b8200e6f9b159aee32bf919b59148172c0fdba8
? <LP3CG@HOTMAIL.COM>
2025-03-20 23:18:10 +0100

    Initial commit

---
+++ Requests/Requests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movies.Contracts.Requests;
+
+public class CreateMovieRequests
+{
+    public required string Title { get; init; }
+    public required int YearOfRelease { get; init; }
+    public required IEnumerable<string> Genres { get; init; } = [];
+}
+
+public class UpdateMovieRequests
+{
+    public required string Title { get; init; }
+    public required int YearOfRelease { get; init; }
+    public required IEnumerable<string> Genres { get; init; } = [];
+}
